f10a59fad588c40bbdec4ab2d3ee4bf67d686377
horstboehse <daniel123@bossmail.de>
2019-04-30 18:33:30 +0200

    Added remaining maps to main menu

--- src/gamemap.js
+++ src/gamemap.js
@@ -13,4 +13,7 @@ GameMap.maps = [
     new GameMap("Hilbert Garden", "R", 0, "maps/hilbertsgarden.jpg", "maps/hilbertsgarden.svg", "This is a Hilbert description"),
     new GameMap("Shooting Range 1", "X", 1, "maps/shootingrange1.png", "maps/shootingrange1.svg", "This is a Shooting Range 1 description"),
     new GameMap("Shooting Range 2", "X", 2, "maps/shootingrange2.png", "maps/shootingrange2.svg", "This is a Shooting Range 2 description"),
+    new GameMap("Gass Ice Islands", "D", 0, "maps/grassiceislands.png", "maps/grassiceislands.svg", "This is a Gass Ice Islands description"),
+    new GameMap("Hackerland", "D", 0, "maps/hackerland.png", "maps/hackerland.svg", "This is a Hackerland description"),
+    new GameMap("Honeycombs", "D", 0, "maps/honeycombs.png", "maps/honeycombs.svg", "This is a Honeycombs description")
 ]
