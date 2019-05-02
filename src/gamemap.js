50bb43559dd77cbf97ce27f3e6063596388facb3
Mirko <mirko.kunze@web.de>
2019-05-02 16:50:22 +0200

    minor tweaks

--- src/gamemap.js
+++ src/gamemap.js
@@ -1,19 +1,19 @@
 
 GameMap = function(name, gameMode, gameLevel, thumbnail, svg, description) {
-    this.name = name;
-    this.gameMode = gameMode;
-    this.gameLevel = gameLevel;
-    this.thumbnail = thumbnail;
-    this.svg = svg;
-    this.description = description;    
+	this.name = name;
+	this.gameMode = gameMode;
+	this.gameLevel = gameLevel;
+	this.thumbnail = thumbnail;
+	this.svg = svg;
+	this.description = description;
 }
 
 GameMap.maps = [
-    new GameMap("Cactus", "R", 0, "maps/cactus.jpg", "maps/cactus.svg", "This is a Cactus description"),
-    new GameMap("Hilbert Garden", "R", 0, "maps/hilbertsgarden.jpg", "maps/hilbertsgarden.svg", "This is a Hilbert description"),
-    new GameMap("Shooting Range 1", "X", 1, "maps/shootingrange1.png", "maps/shootingrange1.svg", "This is a Shooting Range 1 description"),
-    new GameMap("Shooting Range 2", "X", 2, "maps/shootingrange2.png", "maps/shootingrange2.svg", "This is a Shooting Range 2 description"),
-    new GameMap("Gass Ice Islands", "D", 0, "maps/grassiceislands.png", "maps/grassiceislands.svg", "This is a Gass Ice Islands description"),
-    new GameMap("Hackerland", "D", 0, "maps/hackerland.png", "maps/hackerland.svg", "This is a Hackerland description"),
-    new GameMap("Honeycombs", "D", 0, "maps/honeycombs.png", "maps/honeycombs.svg", "This is a Honeycombs description")
+	new GameMap("Cactus", "R", 0, "maps/cactus.jpg", "maps/cactus.svg", "Race through the desert of Molvania!"),
+	new GameMap("Hilbert Garden", "R", 0, "maps/hilbertsgarden.jpg", "maps/hilbertsgarden.svg", "Race through the Hilbert curve!"),
+	new GameMap("Shooting Range 1", "X", 1, "maps/shootingrange1.png", "maps/shootingrange1.svg", "Shoot the targets and avoid touching or shooting the mines!"),
+	new GameMap("Shooting Range 2", "X", 2, "maps/shootingrange2.png", "maps/shootingrange2.svg", "Shoot the targets and avoid touching or shooting the mines!"),
+	new GameMap("Gass Ice Islands", "D", 0, "maps/grassiceislands.png", "maps/grassiceislands.svg", "Deathmatch on Grassice Island"),
+	new GameMap("Hackerland", "D", 0, "maps/hackerland.png", "maps/hackerland.svg", "Deathmatch in Hackerland"),
+	new GameMap("Honeycombs", "D", 0, "maps/honeycombs.png", "maps/honeycombs.svg", "Deathmatch in the BeeHive")
 ]
