91001f9bba4055fd0b9f56f0e930d802bdb6dbea
horstboehse <daniel123@bossmail.de>
2020-02-24 22:25:01 +0100

    Use class construct for classes

--- src/control/gamecontroller.js
+++ src/control/gamecontroller.js
@@ -1,46 +1,49 @@
 
 // depends on scene.js
 
-function GameController(){
-    this.eventSupport = new EventSupport();
-    this.control = new Control();
-}
+class GameController {
 
-GameController.EventTypes = {pause: "pause"}
+    constructor() {
+        this.eventSupport = new EventSupport();
+        this.control = new Control();
+    }
 
-GameController.createControl = function(configValues) { // player config string including name and color
-    var params = configValues.split(',');
-    switch (params[2]) {
-        case 'md':
-            return new MobileDevice(params);
-        case 'ms':
-            return new Mouse(params);
-        case 'gp':
-            return new Gamepad(params);
-        case 'kb':
-            return new Keyboard(params);
-        default:
-            return new GameController(params);
+    static createControl(configValues) { // player config string including name and color
+        const params = configValues.split(',');
+        switch (params[2]) {
+            case 'md':
+                return new MobileDevice(params);
+            case 'ms':
+                return new Mouse(params);
+            case 'gp':
+                return new Gamepad(params);
+            case 'kb':
+                return new Keyboard(params);
+            default:
+                return new GameController(params);
+        }
+    }
+
+    setControl(control) {
+        this.control = control;
+    }
+
+    update() {
+        // Nothing done in the base class. Sub classes have to override this method
+        // TODO: test all the control modalities
+    }
+
+    pausePressed() {
+        this.eventSupport.fireEvent({type: GameController.EventTypes.pause});
+    }
+
+    addEventHandler(handler) {
+        this.eventSupport.addHandler(handler);
+    }
+
+    removeEventHandler(handler) {
+        this.eventSupport.removeHandler(handler);
     }
 }
 
-GameController.prototype.setControl = function(control) {
-    this.control = control;
-}
-
-GameController.prototype.update = function(){
-    // Nothing done in the base class. Sub classes have to override this method
-    // TODO: test all the control modalities
-}
-
-GameController.prototype.pausePressed = function() {
-    this.eventSupport.fireEvent({type: GameController.EventTypes.pause});
-}
-
-GameController.prototype.addEventHandler = function(handler) {
-    this.eventSupport.addHandler(handler);
-}
-
-GameController.prototype.removeEventHandler = function(handler) {
-    this.eventSupport.removeHandler(handler);
-}
+GameController.EventTypes = {pause: "pause"};
