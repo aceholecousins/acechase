fc1e22d5ab80f82e9b676b32629d4c4e3274f116
horstboehse <daniel123@bossmail.de>
2020-03-17 22:32:07 +0100

    Convert AirController to class

--- src/control/aircontroller.js
+++ src/control/aircontroller.js
@@ -1,79 +1,78 @@
-// Inherits from GameController
-"use strict";
-function AirController(device_id) {
-	GameController.call(this);
+class AirController extends GameController {
+	
+	constructor(device_id) {
+		super();
 
-	this.device_id = device_id;
-	this.nickName = "No Name";
-	this.color = new THREE.Color('black');
-	this.connected = true;
-}
-
-AirController.prototype = Object.create(GameController.prototype);
-AirController.prototype.constructor = AirController;
-
-AirController.prototype.onConnect = function() {
-	console.log("AirController re-connected", this.device_id);
-	this.connected = true;
-}
-
-AirController.prototype.onDisconnect = function() {
-	console.log("AirController disconnected", this.device_id);
-	this.connected = false;
-
-	//Reset all control parameters except direction
-	this.control.thrust = 0;	
-	this.control.fire = false;
-	this.control.special = false;
-	this.spin = 0;
-}
-
-AirController.prototype.onMenuMessage = function (navi, eventSupport) {
-	switch (navi) {
-		case "right":
-			eventSupport.fireEvent({ type: "menu", value: "right", controller: this });
-			break;
-		case "left":
-			eventSupport.fireEvent({ type: "menu", value: "left", controller: this });
-			break;
-		case "go":
-			eventSupport.fireEvent({ type: "menu", value: "enter", controller: this });
-			break;
+		this.device_id = device_id;
+		this.nickName = "No Name";
+		this.color = new THREE.Color('black');
+		this.connected = true;
 	}
-}
 
-AirController.prototype.onControllerMessage = function (data) {
-	if(data.fire !== undefined) {
-		this.control.fire = data.fire
-	}
-	if(data.thrust !== undefined) {
-		this.control.thrust = data.thrust;
-	}
-	if (data.direction !== undefined) {
-		this.control.direction = data.direction;
-	}
-	if(data.spin !== undefined) {
-		this.spin = data.spin;
-	}
-}
 
-AirController.prototype.onEventMessage = function (eventType) {
-	if(eventType == "pause") {		
-		this.pausePressed();
+	onConnect() {
+		console.log("AirController re-connected", this.device_id);
+		this.connected = true;
 	}
-}
 
-AirController.prototype.onStateChange = function (data, eventSupport) {
-	const maxValue = 255;
-	let color = data.color;
-	if(color !== undefined) {
-		this.color = new THREE.Color(color.red / maxValue, color.green / maxValue, color.blue / maxValue);
+	onDisconnect() {
+		console.log("AirController disconnected", this.device_id);
+		this.connected = false;
+
+		//Reset all control parameters except direction
+		this.control.thrust = 0;	
+		this.control.fire = false;
+		this.control.special = false;
+		this.spin = 0;
 	}
-	eventSupport.fireEvent({ type: "menu", value: "state", controller: this });
-}
 
-AirController.prototype.update = function () {
-	if (this.spin !== undefined && this.spin != 0) {
-		this.control.direction += this.spin * 5.0 * DT;
+	onMenuMessage (navi, eventSupport) {
+		switch (navi) {
+			case "right":
+				eventSupport.fireEvent({ type: "menu", value: "right", controller: this });
+				break;
+			case "left":
+				eventSupport.fireEvent({ type: "menu", value: "left", controller: this });
+				break;
+			case "go":
+				eventSupport.fireEvent({ type: "menu", value: "enter", controller: this });
+				break;
+		}
+	}
+
+	onControllerMessage(data) {
+		if(data.fire !== undefined) {
+			this.control.fire = data.fire
+		}
+		if(data.thrust !== undefined) {
+			this.control.thrust = data.thrust;
+		}
+		if (data.direction !== undefined) {
+			this.control.direction = data.direction;
+		}
+		if(data.spin !== undefined) {
+			this.spin = data.spin;
+		}
+	}
+
+	onEventMessage(eventType) {
+		if(eventType == "pause") {		
+			this.pausePressed();
+		}
+	}
+
+	onStateChange(data, eventSupport) {
+		const maxValue = 255;
+		let color = data.color;
+		if(color !== undefined) {
+			this.color = new THREE.Color(color.red / maxValue, color.green / maxValue, color.blue / maxValue);
+		}
+		eventSupport.fireEvent({ type: "menu", value: "state", controller: this });
+	}
+
+	update() {
+		if (this.spin !== undefined && this.spin != 0) {
+			this.control.direction += this.spin * 5.0 * DT;
+		}
 	}
 }
