3747b18e87f3a7b3fbc319519ca01a39e2e1b36c
horstboehse <daniel123@bossmail.de>
2020-03-17 22:43:29 +0100

    Convert ScreenControl to class

--- src/control/screencontrol.js
+++ src/control/screencontrol.js
@@ -1,36 +1,34 @@
-
-function ScreenControl() {
+class ScreenControl {
 	
-}
+	static enterFullScreen() {
+		var el = document.documentElement;
+		var requestMethod = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullscreen;
 
-ScreenControl.enterFullScreen = function() {
-	var el = document.documentElement;
-	var requestMethod = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullscreen;
-
-	if (requestMethod) { // Native full screen.
-		requestMethod.call(el);
+		if (requestMethod) { // Native full screen.
+			requestMethod.call(el);
+		}
 	}
-}
 
-ScreenControl.lockScreenToLandscape = function() {
-	const orientationToLock = ScreenControl.getCurrentOrientation();
-	console.log("Try to lock screen to current orientation: " + orientationToLock);
-	screen.lockOrientationUniversal = 			
-		screen.lockOrientation || 
-		screen.mozLockOrientation || 
-		screen.msLockOrientation;
-	var lockResult;
-	if(screen.lockOrientationUniversal) {
-		lockResult = screen.lockOrientationUniversal(orientationToLock);
-	} else if(screen.orientation.lock) {
-		lockResult = screen.orientation.lock(orientationToLock);
+	static lockScreenToLandscape() {
+		const orientationToLock = ScreenControl.getCurrentOrientation();
+		console.log("Try to lock screen to current orientation: " + orientationToLock);
+		screen.lockOrientationUniversal = 			
+			screen.lockOrientation || 
+			screen.mozLockOrientation || 
+			screen.msLockOrientation;
+		var lockResult;
+		if(screen.lockOrientationUniversal) {
+			lockResult = screen.lockOrientationUniversal(orientationToLock);
+		} else if(screen.orientation.lock) {
+			lockResult = screen.orientation.lock(orientationToLock);
+		}
 	}
-}
 
-ScreenControl.getCurrentOrientation = function() {
-	screen.orientationUniversal = 
-			screen.orientation || 
-			screen.mozOrientation || 
-			screen.msOrientation;	
-	return screen.orientationUniversal.type;;
+	static getCurrentOrientation() {
+		screen.orientationUniversal = 
+				screen.orientation || 
+				screen.mozOrientation || 
+				screen.msOrientation;	
+		return screen.orientationUniversal.type;
+	}
 }
