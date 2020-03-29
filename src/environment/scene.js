b6436166945b8a79f6d1cdcc7e92ed6cc6b966ac
Mirko <mirko.kunze@web.de>
2020-03-29 16:46:35 +0200

    temple texture

--- src/environment/scene.js
+++ src/environment/scene.js
@@ -11,15 +11,10 @@ Scene.init = function() {
     Scene.renderer.setClearColor(0x000000);
     document.body.appendChild(Scene.renderer.domElement); // creates canvas on page into which the scene will be rendered
 
-    ambientLight = new THREE.AmbientLight(0x404050);
+    ambientLight = new THREE.AmbientLight(0x555555);
     Scene.graphicsScene.add(ambientLight);
 
-    sunLight = new THREE.DirectionalLight(0xffffff, 0.8);
+    sunLight = new THREE.DirectionalLight(0xeeeeee, 0.8);
     sunLight.position.set(-1, 1, 1).normalize();
     Scene.graphicsScene.add(sunLight);
 }
-
-
-
-
-
